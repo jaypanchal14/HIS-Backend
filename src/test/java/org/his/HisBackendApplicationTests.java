3ed41d513e3b21d4d02a3bb418ac00f48b96e5fd
jaypanchal14 <jay1402panchal@gmail.com>
2024-03-28 18:14:47 +0530

    General configuration

---
+++ src/test/java/org/his/HisBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package org.his;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class HisBackendApplicationTests {
+
+
+
+}
