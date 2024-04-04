1ebb15492a4c060dfabc88340d189bf6c825ce26
JD2582 <darshit.jakhaniya@iiitb.ac.in>
2024-04-04 20:50:34 +0530

    Endpoint added for view One Live Patient

---
+++ src/main/java/org/his/controller/DoctorController.java
@@ -0,0 +1,38 @@
+package org.his.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.his.bean.PatientResponse;
+import org.his.service.DoctorService;
+import org.his.service.NurseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/doc")
+@CrossOrigin
+@Slf4j
+public class DoctorController {
+
+    private final DoctorService doctorService;
+
+    @Autowired
+    public DoctorController(DoctorService doctorService) {
+        this.doctorService = doctorService;
+    }
+
+    @GetMapping("/viewPastPatients")
+    public ResponseEntity<PatientResponse> viewLivePatients(
+//            @RequestParam(name="role") String role,
+//            @RequestParam(name="id") String id
+    ){
+        PatientResponse response = doctorService.viewPastPatients();
+        log.info(response.toString());
+        if (response.getResponse() != null) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
---
+++ src/main/java/org/his/service/DoctorService.java
@@ -0,0 +1,72 @@
+package org.his.service;
+
+import org.his.bean.PatientDetail;
+import org.his.bean.PatientResponse;
+import org.his.entity.user.Patient;
+import org.his.repo.user.DoctorRepo;
+import org.his.repo.user.PatientRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DoctorService {
+
+    private final DoctorRepo doctorRepo;
+    private final PatientRepo patientRepo;
+
+    @Autowired
+    public DoctorService(DoctorRepo doctorRepo, PatientRepo patientRepo) {
+
+        this.doctorRepo= doctorRepo;
+        this.patientRepo = patientRepo;
+    }
+
+
+    public PatientResponse viewPastPatients() {
+        PatientResponse response = new PatientResponse();
+
+
+        // Check if the role is supported
+//        if (!role.equalsIgnoreCase("DOCTOR")) {
+//            response.setError("Role not supported.");
+//            return response;
+//        }
+
+        List<Patient> patients = new ArrayList<>();
+        // Check if the nurse is head nurse
+
+        patients.addAll(patientRepo.findAll());
+        if (patients.isEmpty()) {
+            response.setError("No nurses available.");
+            return response;
+        }
+        List<PatientDetail> patientDetails = new ArrayList<>();
+
+        // Iterate through the list of nurses and map their details
+        for (Patient patient : patients) {
+
+                PatientDetail detail = new PatientDetail();
+                detail.setId(patient.getId());
+                detail.setFirstName(patient.getFirstName());
+                detail.setLastName(patient.getLastName());
+//                detail.setEmail(patient.getEmail());
+//                detail.setPhone(patient.getPhoneNumber());
+//                detail.setGender(patient.getGender());
+//                detail.setBlood(patient.getBloodGroup());
+//                detail.setAddress(patient.getAddress());
+//                detail.setBirthDate(patient.getBirthDate().toString());
+
+
+                patientDetails.add(detail);
+
+        }
+
+        response.setResponse(patientDetails);
+
+        return response;
+    }
+
+
+}
